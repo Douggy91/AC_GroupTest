ad6c6b04fa4092debe83ffedb41721076814e142
Douggy91 <ejrwls91@gmail.com>
2023-05-31 01:11:08 +0900

    get_all_krw => get_tools add get_timestamp

--- custom_module/get_tools.py
+++ custom_module/get_tools.py
@@ -1,4 +1,8 @@
 import requests, redis, json
+import time 
+
+def get_timestamp(time_val):
+    return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(time_val))
 
 def get_all_krw(host,port,db):
     r = redis.Redis(host=host, port=port, db=db)
@@ -15,5 +19,3 @@ def get_all_krw(host,port,db):
             r.append(pair[0],","+"\n" + json.dumps(pairs_list[-1][1])+":"+json.dumps(pair[1]))
 
 # get_all_krw("localhost",6379,0)
-
-
